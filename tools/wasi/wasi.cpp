ccd363e8305bf1e5f3d6578ac37c529aad2a90e3
Alex Beregszaszi <alex@rtfs.hu>
2020-05-22 11:36:13 +0100

    wasi: implement initialisation

--- tools/wasi/wasi.cpp
+++ tools/wasi/wasi.cpp
@@ -11,10 +11,32 @@
 
 namespace
 {
+// Global state.
+// NOTE: we do not care about uvwasi_destroy(), because it only clears file descriptors (currently)
+// and we are a single-run tool. This may change in the future and should reevaluate.
+uvwasi_t state;
+
 bool run(int argc, const char** argv)
 {
-    (void)argc;
-    (void)argv;
+    uvwasi_options_t options = {
+        3,           // sizeof fd_table
+        0, nullptr,  // NOTE: no remappings
+        static_cast<uvwasi_size_t>(argc), argv,
+        nullptr,  // TODO: support env
+        0, 1, 2,
+        nullptr,  // NOTE: no special allocator
+    };
+
+    uvwasi_errno_t err = uvwasi_init(&state, &options);
+    if (err != UVWASI_ESUCCESS)
+    {
+        std::cerr << "Failed to initialise UVWASI: " << uvwasi_embedder_err_code_to_string(err)
+                  << "\n";
+        return false;
+    }
+
+    // TODO: load wasm file
+
     return false;
 }
 }  // namespace
