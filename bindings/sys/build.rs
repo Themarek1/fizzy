fd813c022adc53a8026a132cbc4c565e3ee5ce40
Alex Beregszaszi <alex@rtfs.hu>
2020-09-21 20:41:32 +0100

    add cmake build

--- bindings/sys/build.rs
+++ bindings/sys/build.rs
@@ -3,13 +3,31 @@
 // SPDX-License-Identifier: Apache-2.0
 
 extern crate bindgen;
+extern crate cmake;
+
+use cmake::Config;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let src = "fizzy";
+
+    let dst = Config::new(src)
+        .define("FIZZY_TESTING", "OFF")
+        .define("FIZZY_FUZZING", "OFF")
+        .build();
+
+    println!("cargo:rustc-link-lib=static={}", "fizzy");
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+
+    // We need to link against C++ std lib
+    if let Some(cpp_stdlib) = get_cpp_stdlib() {
+        println!("cargo:rustc-link-lib={}", cpp_stdlib);
+    }
+
     let bindings = bindgen::Builder::default()
-        .header("fizzy.h")
+        .header(format!("{}/include/fizzy/fizzy.h", src))
         // See https://github.com/rust-lang-nursery/rust-bindgen/issues/947
         .trust_clang_mangling(false)
         .generate_comments(true)
@@ -38,4 +56,21 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+// See https://github.com/alexcrichton/gcc-rs/blob/88ac58e25/src/lib.rs#L1197
+fn get_cpp_stdlib() -> Option<String> {
+    env::var("TARGET").ok().and_then(|target| {
+        if target.contains("msvc") {
+            None
+        } else if target.contains("darwin") {
+            Some("c++".to_string())
+        } else if target.contains("freebsd") {
+            Some("c++".to_string())
+        } else if target.contains("musl") {
+            Some("static=stdc++".to_string())
+        } else {
+            Some("stdc++".to_string())
+        }
+    })
 }
