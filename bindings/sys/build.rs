92e912798b9d9a20b0ae2f13ec5a8ec2e154bbc0
Alex Beregszaszi <alex@rtfs.hu>
2020-09-21 21:55:52 +0100

    f

--- bindings/sys/build.rs
+++ bindings/sys/build.rs
@@ -34,20 +34,7 @@ fn main() {
         .generate_comments(true)
         // https://github.com/rust-lang-nursery/rust-bindgen/issues/947#issuecomment-327100002
         .layout_tests(false)
-        // do not generate an empty enum for EVMC_ABI_VERSION
-        .constified_enum("")
-        // generate Rust enums for each evmc enum
-        .rustified_enum("*")
-        // force deriving the Hash trait on basic types (address, bytes32)
-        .derive_hash(true)
-        // force deriving the PratialEq trait on basic types (address, bytes32)
-        .derive_partialeq(true)
-        //        .whitelist_type("fizzy_*")
-        //        .whitelist_function("fizzy_*")
-        //        .opaque_type("evmc_host_context")
-        //        .whitelist_type("evmc_.*")
-        //        .whitelist_function("evmc_.*")
-        //        .whitelist_var("EVMC_ABI_VERSION")
+        .whitelist_function("fizzy_.*")
         // TODO: consider removing this
         .size_t_is_usize(true)
         .generate()
@@ -56,7 +43,7 @@ fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .expect("Could not write bindings");
 }
 
 // See https://github.com/alexcrichton/gcc-rs/blob/88ac58e25/src/lib.rs#L1197
