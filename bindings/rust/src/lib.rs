1c34eaec0ab9f433ec0dba89d46656ca02f1323a
Alex Beregszaszi <alex@rtfs.hu>
2020-09-21 22:48:33 +0100

    add module wrapper

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -4,8 +4,31 @@
 
 mod sys;
 
+use std::ptr::NonNull;
+
 pub fn validate(input: &[u8]) -> bool {
     unsafe { sys::fizzy_validate(input.as_ptr(), input.len()) }
+}
+
+pub struct Module {
+    ptr: NonNull<sys::fizzy_module>,
+}
+
+impl Drop for Module {
+    fn drop(&mut self) {
+        println!("Dropping module");
+        unsafe { sys::fizzy_free_module(self.ptr.as_ptr()) }
+    }
+}
+
+pub fn parse(input: &[u8]) -> Option<Module> {
+    let ptr = unsafe { sys::fizzy_parse(input.as_ptr(), input.len()) };
+    if ptr.is_null() {
+        return None;
+    }
+    Some(Module {
+        ptr: unsafe { NonNull::new_unchecked(ptr) },
+    })
 }
 
 #[cfg(test)]
@@ -14,8 +37,20 @@ mod tests {
 
     #[test]
     fn validate_wasm() {
-        assert_eq!(validate(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00]), true);
-        assert_eq!(validate(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x01]), false);
+        assert_eq!(
+            validate(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00]),
+            true
+        );
+        assert_eq!(
+            validate(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x01]),
+            false
+        );
         assert_eq!(validate(&[0x00]), false);
+    }
+
+    #[test]
+    fn parse_wasm() {
+        assert!(parse(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00]).is_some());
+        assert!(parse(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x01]).is_none());
     }
 }
