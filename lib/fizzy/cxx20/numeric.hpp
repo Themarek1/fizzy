d7fd40e769ddbd524a372325c3a8bdf75b5b0344
Alex Beregszaszi <alex@rtfs.hu>
2020-09-22 16:03:17 +0100

    f

---
+++ lib/fizzy/cxx20/numeric.hpp
@@ -0,0 +1,34 @@
+// Fizzy: A fast WebAssembly interpreter
+// Copyright 2019-2020 The Fizzy Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#if __cplusplus > 201703L
+
+#include <bit>
+
+namespace fizzy
+{
+template <typename T>
+using popcount = std::popcount<T>;
+}
+
+#else
+
+#include <cstdint>
+
+namespace fizzy
+{
+constexpr int popcount(uint32_t x) noexcept
+{
+    return __builtin_popcount(x);
+}
+
+constexpr int popcount(uint64_t x) noexcept
+{
+    return __builtin_popcountll(x);
+}
+}  // namespace fizzy
+
+#endif /* __cplusplus > 201703L */
